602595cf91470e34cd45a150c29ed8bc37bc4fb3
Richard Bunker <32656066+richardbunker@users.noreply.github.com>
2023-07-18 11:44:51 +1000

    Create groupBy.js

---
+++ groupBy.js
@@ -0,0 +1,23 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const groupBy = (arr, callback) => {
+  const map = {};
+  for (let index = 0; index < arr.length; index++) {
+    const key = callback(arr[index], index);
+    if (!map[key]) {
+      map[key] = [arr[index]];
+    } else {
+      map[key] = [...map[key], arr[index]];
+    }
+  }
+  return map;
+};
+
+Object.groupBy = groupBy;
+
+const result = Object.groupBy(arr, (num, index) => {
+  return num % 2 === 0 ? "even" : "odd";
+});
+
+console.log(result);
+// {even: [2, 4, 6, 8, 10], odd: [1, 3, 5, 7, 9]}
