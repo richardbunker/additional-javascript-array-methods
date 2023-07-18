b38dc27500ce7cc72ea20c7fdf997f163979ef98
Richard Bunker <32656066+richardbunker@users.noreply.github.com>
2023-07-18 11:49:51 +1000

    Update groupBy.js

--- groupBy.js
+++ groupBy.js
@@ -20,4 +20,9 @@ const result = Object.groupBy(arr, (num, index) => {
 });
 
 console.log(result);
-// {even: [2, 4, 6, 8, 10], odd: [1, 3, 5, 7, 9]}
+// Expected Output
+// { 
+//   even: [2, 4, 6, 8, 10],
+//   odd: [1, 3, 5, 7, 9]
+// }
+
